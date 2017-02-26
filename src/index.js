f73754e7ef00c50e0c92c9698e6a1fe4c42521f7
mubashiralisiddiqui <mubashir22ali@gmail.com>
2017-02-26 22:08:49 +0500

    counter app without redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import  App  from "./components/App";
+/*import  App  from "./components/App";
  import  Home  from "./components/home";
   import  About  from "./components/about";
 
 // import  About  from "./";
 import  Contact  from "./components/contact";
 import './index.css';
+
 
 import {Router,Route,hashHistory,IndexRoute,browserHistory}from 'react-router';
 ReactDOM.render(
@@ -21,5 +22,37 @@ ReactDOM.render(
     </Route>
   </Router>
   ,
+  */
+
+  class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      counter: 0
+    }
+    this.addOne = this.addOne.bind(this)
+  }
+  
+  addOne() {
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
+  
+  render() {
+    return (
+      <div>
+        <button
+          onClick={ this.addOne }>
+          Increment
+        </button>
+        { this.state.counter }
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+<App/>,
   document.getElementById('root')
 );
